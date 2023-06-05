ecf53113e7c22fba63d515542cef6155fc964a0c
hogrider65 <126177491+hogrider65@users.noreply.github.com>
2023-06-05 16:50:43 +0200

    Add files via upload

---
+++ home-page.js
@@ -0,0 +1,9 @@
+/* mette la larghezza del pannello a 250px (aperto)*/
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "350px";
+  }
+  
+  /* mette la rghezza a 0 (chiuso) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
